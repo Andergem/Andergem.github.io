235515e81388fcfd7c24420083ec8cf326a8980e
Anderson Lopez Melendres <anderslop10@outlook.com>
2020-10-04 13:52:17 -0500

    Agregando stilos

--- src/componentes/badge.js
+++ src/componentes/badge.js
@@ -1,25 +1,27 @@
 import React from 'react';
+
+import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component{
     //todos los componentes requieren por lo menos un metodo que es obligatorio y ese es el render()
     render(){
-        return <div>
-            <div>
+        return <div className="Badge">
+            <div className="Badge__header">
                 <img src={confLogo} alt="logo de la conferencia"/>
             </div>
 
-            <div>
-                <img src="https://www.gravatar.com/avatar?d=identicon" alt="avatar"/>
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="avatar"/>
                 <h1>Anderson <br/> Lopez</h1>
             </div>
 
-            <div>
-                <p>Fronted Enginier</p>
-                <p>@anderlop10</p>
+            <div className="Badge__section-info">
+                <h3>Fronted Engineer</h3>
+                <div>@anderlop10</div>
             </div>
 
-            <div>
+            <div className="Badge__footer">
                 #platzi
             </div>
         </div>;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 import Badge from './componentes/badge';
 
  
